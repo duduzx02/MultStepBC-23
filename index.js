09267a36d987e8f6ec01f384d323496a93be0ac0
duduzx02 <duduzx02@gmail.com>
2023-06-17 23:39:36 -0300

    Form do JS 'ok'

--- index.js
+++ index.js
@@ -1,4 +1,67 @@
+let currentStep = 0
+const formSteps = document.querySelectorAll('.form-step')
 const form = document.querySelector('form')
+
+/*Steps*/
+form.addEventListener('click', (e) => {
+    if(!e.target.matches('[data-action]')){
+        return 
+    }
+
+    const actions ={
+        next() {
+            if(!isValidInputs()){
+                return 
+            }
+            currentStep++
+        },
+        prev() {
+            currentStep--
+        }
+    }
+
+    const action = e.target.dataset.action
+    actions[action]()
+    updateActiveStep()
+    updateProgressStep()
+})
+
+/*Envio do formulário */
 form.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    const data = new FormData(form)
+    alert(`Obrigado ${data.get('name')}!`)
 })
+
+/*Update steps*/
+function updateActiveStep(){
+    formSteps.forEach(step => {
+        step.classList.remove('active')
+        formSteps[currentStep].classList.add('active')
+    })
+}
+
+const progressSteps = document.querySelectorAll('.step-progress [data-step]')
+
+function updateProgressStep() {
+    progressSteps.forEach((step, idx) =>{
+        step.classList.remove('active')
+        step.classList.remove('done')
+
+        if(idx < currentStep + 1){
+            step.classList.add('active')
+        }
+
+        if(idx < currentStep){
+            step.classList.add('done')
+        }
+    })
+}
+
+/*Validation */
+function isValidInputs(){
+    const currentFormStep = formSteps[currentStep]
+    const formFields = [...currentFormStep.querySelectorAll('input'), ...currentFormStep.querySelectorAll('textarea')]
+    return formFields.every((input) => input.reportValidity())
+}
